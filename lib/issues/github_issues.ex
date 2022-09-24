59638b84bcecfa3d6a5cb0748e2f5f2e1447b6dd
dzdn <jdustinwilliams@gmail.com>
2022-09-24 16:43:22 -0400

    Make Github URL configurable.

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,14 +1,15 @@
 defmodule Issues.GithubIssues do
   @user_agent  [ { "User-agent", "Elixir dzdn@example.com"} ]
+  @github_url Application.get_env(:issues, :github_url)
+
+  def issues_url(user, project) do
+    "#{@github_url}/repos/#{user}/#{project}/issues"
+  end
 
   def fetch(user, project) do
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response
-  end
-
-  def issues_url(user, project) do
-    "https://api.github.com/repos/#{user}/#{project}/issues"
   end
 
   def handle_response({ _, %{status_code: status_code, body: body }}) do
