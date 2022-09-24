2eb24749915319e6223ef538037a0fd2492d1b7b
dzdn <jdustinwilliams@gmail.com>
2022-09-24 15:08:30 -0400

    Implement Issues.CLI.parse_args function.

---
+++ lib/issues/cli.ex
@@ -0,0 +1,53 @@
+defmodule Issues.CLI do
+
+  @default_count 4
+
+  @moduledoc """
+  Handle the command line parsing and the dispatch to
+  the various functions that end up generating a
+  table of the last _n_ issues in a github project
+  """
+
+  def run(argv) do
+    parse_args(argv)
+  end
+
+  @doc """
+  `argv` can he -h or --help, which returns :help.
+
+  Otherwise it is a github user name, project name, and (optionally)
+  the number of entries to format.any()
+
+  Return a tuple of `{ user, project, count }`, or `:help` if help was given.
+  """
+  def parse_args(argv) do
+    parse = OptionParser.parse(argv, switches: [ help:  :boolean],
+                                     aliases:  [ h:     :help   ])
+    |> elem(1)
+    |> args_to_internal_representation()
+  end
+
+    # case parse do
+    # { [ help: true], _, _ }
+      # -> :help
+
+      # { _, [ user, project, count ], _ }
+      # -> { user, project, String.to_integer(count) }
+
+    # { _, [ user, project ], _ }
+      # -> { user, project, @default_count }
+    # end
+  # end
+
+  def args_to_internal_representation([user, project, count]) do
+    {user, project, String.to_integer(count)}
+  end
+
+  def args_to_internal_representation([user, project]) do
+    { user, project, @default_count}
+  end
+
+  def args_to_internal_representation(_) do
+    :help
+  end
+end
